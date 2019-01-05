d3def9b27476bbf1cbc8c450619dfaba2eab4921
Danni <dannichen236@gmail.com>
2019-01-04 21:50:39 -0800

    The project is functioning, all the tasks is completed

--- Uswer_Drawing/Controllers/ViewController.swift
+++ Uswer_Drawing/Controllers/ViewController.swift
@@ -11,6 +11,29 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    let colorPalette:UIStackView = {
+       
+        
+        
+        let blue = UIButton().createButtonWithColor(color: .blue)
+        let red = UIButton().createButtonWithColor(color: .red)
+        let yellow = UIButton().createButtonWithColor(color: .yellow)
+        let black = UIButton().createButtonWithColor(color: .black)
+        [blue,red,yellow,black].forEach({ (button) in
+            button.addTarget(self,action: #selector(pickColor(sender:)), for: .touchUpInside)
+        })
+        
+        let colorPallete = UIStackView(arrangedSubviews: [red,yellow,black,blue])
+        colorPallete.distribution = .fillEqually
+        colorPallete.spacing = 5
+        colorPallete.translatesAutoresizingMaskIntoConstraints = false
+        return colorPallete
+    }()
+    
+    @objc fileprivate func pickColor(sender:UIButton){
+        guard let buttonColor = sender.backgroundColor?.cgColor else{ return }
+        drawingView.pickedColor = buttonColor
+    }
     let thinLabel:UILabel = {
         let label = UILabel()
         label.text = "thin"
@@ -33,34 +56,67 @@ class ViewController: UIViewController {
         return label
     }()
     
-    let drawAbleView:UIView = {
-        let view = UIView()
+    let setClearButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Clear", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 28)
+        button.addTarget(self,action: #selector(clearDrawingView), for: .touchUpInside)
+        return button
+    }()
+    @objc fileprivate func clearDrawingView(){
+        drawingView.curves = [curve]()
+        drawingView.setNeedsDisplay()
+    }
+    
+    let undrawButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("undraw", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 28)
+        button.addTarget(self, action: #selector(undraw), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc fileprivate func undraw(){
+        drawingView.curves.popLast()
+        drawingView.setNeedsDisplay()
+    }
+
+    let drawingView:drawableView = {
+        let view = drawableView()
+        view.backgroundColor = .white
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .blue
+        view.isUserInteractionEnabled = true
         return view
     }()
+    
+    @objc fileprivate func handleThickNess(slider:UISlider,event:UIEvent){
+        print(slider.value)
+        drawingView.pickedWidth = CGFloat(slider.value)
+    }
     
     lazy var thicknessPicker:UISlider = {
         let slider = UISlider()
         slider.translatesAutoresizingMaskIntoConstraints = false
         slider.isContinuous = true
-        slider.minimumValue = 0
-        slider.maximumValue = 2
+        slider.minimumValue = 5
+        slider.maximumValue = 15
         slider.tintColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
         slider.thumbTintColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
         
         // TODO:  add target to handle sliding adjusting the borderwidth of drawing to corresponding value
-    
+        slider.addTarget(self, action: #selector(handleThickNess), for: .touchUpInside)
         return slider
     }()
     
     fileprivate func configureDrableView() {
-        view.addSubview(drawAbleView)
-
-        [drawAbleView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-         drawAbleView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-         drawAbleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-         drawAbleView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.65)].forEach { (constraint) in
+        view.addSubview(drawingView)
+    
+        [drawingView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+         drawingView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+         drawingView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+         drawingView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.75)].forEach { (constraint) in
             constraint.isActive = true
         }
     }
@@ -71,9 +127,9 @@ class ViewController: UIViewController {
         pickerStackView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(pickerStackView)
         [pickerStackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-         pickerStackView.topAnchor.constraint(lessThanOrEqualTo: drawAbleView.bottomAnchor, constant: 15),
+         pickerStackView.topAnchor.constraint(lessThanOrEqualTo: drawingView.bottomAnchor, constant: 10),
          pickerStackView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8),
-         pickerStackView.heightAnchor.constraint(equalToConstant: 30)].forEach { (constraint) in
+         pickerStackView.heightAnchor.constraint(equalToConstant: 20)].forEach { (constraint) in
             constraint.isActive = true
         }
         
@@ -88,13 +144,100 @@ class ViewController: UIViewController {
         
     }
     
+    fileprivate func configureColorPalette() {
+        view.addSubview(colorPalette)
+        
+        colorPalette.topAnchor.constraint(equalTo: thicknessPicker.bottomAnchor, constant: 10).isActive = true
+        colorPalette.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        colorPalette.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8).isActive = true
+        colorPalette.heightAnchor.constraint(equalToConstant: 25).isActive = true
+    }
+    
+    fileprivate func configureFunctionalButton() {
+        let functionalButtonStack = UIStackView(arrangedSubviews: [undrawButton,setClearButton])
+        functionalButtonStack.translatesAutoresizingMaskIntoConstraints = false
+        functionalButtonStack.distribution = .fillEqually
+        functionalButtonStack.axis = .vertical
+        functionalButtonStack.spacing = 5
+        
+        view.addSubview(functionalButtonStack)
+        functionalButtonStack.topAnchor.constraint(equalTo: colorPalette.topAnchor,constant:30).isActive = true
+        functionalButtonStack.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        functionalButtonStack.widthAnchor.constraint(lessThanOrEqualToConstant: 100).isActive = true
+        functionalButtonStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant:-10).isActive = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureDrableView()
         configureThicknessPicker()
+        configureColorPalette()
+        configureFunctionalButton()
+        
         
     }
 
 
 }
 
+struct curve {
+    var color:CGColor = UIColor.black.cgColor //default color
+    var points = [CGPoint]()
+    var width:CGFloat = 5 //default width
+}
+
+class drawableView:UIView{
+    var curves = [curve]()
+    var pickedColor = UIColor.black.cgColor
+    var pickedWidth:CGFloat = 5
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard let context = UIGraphicsGetCurrentContext() else{ return }
+        context.setLineCap(CGLineCap.butt)
+        for curve in curves{
+            let color = curve.color
+            let points = curve.points
+            let width = curve.width
+            for point in points{
+                if point == points.first{
+                    context.move(to: point)
+                }
+                else{
+                    context.addLine(to: point)
+                }
+            }
+            context.setLineWidth(CGFloat(width))
+            context.setStrokeColor(color)
+            context.strokePath()
+        }
+    }
+    
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        var newCurve = curve()
+        newCurve.color = pickedColor
+        newCurve.width = pickedWidth
+        curves.append(newCurve)
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else{ return }
+        
+        guard var last = curves.popLast() else { return }
+        last.points.append(touch.location(in:self))
+        curves.append(last)
+        setNeedsDisplay()
+    }
+}
+
+extension UIButton{
+    func createButtonWithColor(color:UIColor)->UIButton{
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.masksToBounds = true
+        button.backgroundColor = color
+        button.showsTouchWhenHighlighted = true
+        return button
+    }
+    
+    
+}
